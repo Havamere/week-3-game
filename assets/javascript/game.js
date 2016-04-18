bdd88e285471c58a903a6930392f621744ad41f6
Kyle Rinehart <Kyle Rinehart>
2016-04-17 20:17:22 -0400

    attempted logic of game started, still not projecting mystery word correctly.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,36 @@
-var mysteryWords = ["bard","beholder","cleric","dragon","druid","kobold","mage","monk","orc","paladin","ranger","rogue","skeletons","vampire","warrior"];
+var words = ["bard","beholder","cleric","dragon","druid","kobold","mage","monk","orc","paladin","ranger","rogue","skeletons","vampire","warrior"];
 
 	// Create code to randomly choose one of the mystery words 
-var wordChoice = mysteryWords[Math.floor(Math.random()*mysteryWords.length)];
+var wordChoice = words[Math.floor(Math.random()*words.length)];
+console.log(wordChoice);
 
+var mysteryWord = [];
+mysteryWord.length = wordChoice.length;
 
+window.onload = function () {
+	document.getElementById("#mystery-word").innerHTML=wordChoice;
+}
+var guessedLetters = [];
 
+var wrongGuesses = 0;
 
 document.onkeyup = function(event) {
 	// Determines which exact key was selected. Make it lowercase
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(userGuess);
+	guessedLetters.push(userGuess);
 
+	for (var a=0; a<guessedLetters.length; a++) {
+		if (userGuess == a) {
+			guessedLetters.pop(userGuess);
+		} else {
+			for (var b=0; b<wordChoice.length; b++) {
+				if (userGuess == b) {
+					mysteryWord[b] = userGuess;
+				} else {
+					wrongGuesses++;
+				}
+			}
+		}	
+	}
 }
