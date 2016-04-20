fa7c4ac759af9bf6c182f2eae814e2c9b70386d0
Kyle Rinehart <Kyle Rinehart>
2016-04-19 23:08:45 -0400

    Major clean up to javascript file, logic still doesn't cover words with repeating letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,3 @@
-// var words = ["bard","beholder","cleric","dragon","druid","kobold","mage","monk","orc","paladin","ranger","rogue","skeletons","vampire","warrior"];
-
-// var rewardsObj = {
-//  	bard : "assets/images/bard.jpg",
-//  	beholder : "assets/images/beholder.jpg",
-//  	cleric : "assets/images/cleric.jpg",
-//  	dragon : "assets/images/dragon.jpg",
-//  	druid : "assets/images/druid.jpg",
-//  	kobold : "assets/images/kobold.jpg",
-//  	mage : "assets/images/mage.jpg",
-//  	monk : "assets/images/monk.jpg",
-//  	orc : "assets/images/orc.jpg",
-//  	paladin : "assets/images/paladin.jpg",
-//  	ranger : "assets/images/ranger.jpg",
-//  	rogue : "assets/images/rogue.jpg",
-//  	skeletons : "assets/images/skeletons.jpg",
-//  	vampire : "assets/images/vampire.jpg",
-//  	warrior : "assets/images/warrior.jpg",
-//  }
-
  	// Words and img source to play the game with
 var rewardsArr = [
 	{word: "bard" , source: "assets/images/bard.jpg"},
@@ -38,12 +18,11 @@ var rewardsArr = [
 ];
 
 	// Create code to randomly choose one of the mystery words 
-//var wordChoice = words[Math.floor(Math.random()*words.length)];
- var compChoice = [Math.floor(Math.random()*rewardsArr.length)];
- var wordChoice = rewardsArr[compChoice].word;
+var compChoice = [Math.floor(Math.random()*rewardsArr.length)];
+var wordChoice = rewardsArr[compChoice].word;
+//console.log(wordChoice);
 
-console.log(wordChoice)
-
+	//Shows user correct guesses.
 var mysteryWord = [];
 mysteryWord.length = wordChoice.length;
 
@@ -52,43 +31,28 @@ document.querySelector("#reward").src=rewardsArr[compChoice].source;
 window.onload = function () {
 	document.querySelector("#mystery-word").innerHTML=mysteryWord;
 }
-// counters for game updates
-var counter = 0;
+// counter needed to account for loss
 var wrongGuesses = 0;
 
 document.onkeyup = function(event) {
 	// Determines which exact key was selected. Makes it lowercase
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(userGuess);
+	// Accounts for loss condition
 	if (wrongGuesses == 8) {
 		document.querySelector("#mystery-word").innerHTML = "You have lost.";
+	// Accounts for winning condition
 	} else if (mysteryWord.join("") == wordChoice) {
 		document.querySelector("#reward").src=rewardsArr[compChoice].source;
 		document.querySelector("#mystery-word").innerHTML = "You have won!";
-	} else if (wordChoice.indexOf(userGuess) >= 0) {	// attempt use of indexOf and/or charAt
+	// Checks if user entry is a match to any letter in  the mystery word and updates html if it is
+	} else if (wordChoice.indexOf(userGuess) >= 0) {
 		mysteryWord[wordChoice.indexOf(userGuess)] = userGuess;
 		document.querySelector("#mystery-word").innerHTML = mysteryWord;
-		console.log(wordChoice.indexOf(userGuess));
+		//console.log(wordChoice.indexOf(userGuess));
+	// Covers letters that don't match any letters in the mystery word
 	} else {
 		wrongGuesses++;
 		console.log(wrongGuesses);
-		}
 	}
 }
-// 	console.log(guessedLetters);
-// 	wrongGuesses = wrongGuesses + counter;
-// 	for (var d=0; d<mysteryWord.length; d++){
-// 		display = display + mysteryWord[d];
-// 	}
-// 	document.querySelector("#mystery-word").innerHTML=display;
-// }
-// 		for (var b=0; b<wordChoice.length; b++) {
-// 			if (userGuess == wordChoice.atChar(b)) {
-// 				mysteryWord[b] = userGuess;
-// 				counter = 0;
-// 				console.log(mysteryWord);
-// 			} 
-// 		}
-// 	}	
-// }
-//}
