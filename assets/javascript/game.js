4f1f2c8c74a6fbdbc5f969ee12164cf29b615ac0
Kyle Rinehart <Kyle Rinehart>
2016-04-20 18:23:02 -0400

    attempting js logic fix to repeating letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,27 +30,37 @@ window.onload = function () {
 	document.querySelector("#mystery-word").innerHTML=mysteryWord;
 }
 // counter needed to account for loss
-var wrongGuesses = 0;
+var wrongGuesses = 8;
+
+var mainDiv = document.querySelector(".main-section");
+var guessesLeftDiv = document.createElement("div");
+mainDiv.appendChild(guessesLeftDiv);
+guessesLeftDiv.setAttribute("id","guesses-left")
 
 document.onkeyup = function(event) {
 	// Determines which exact key was selected. Makes it lowercase
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(userGuess);
+	if (userGuess){
+		for (var i = 0; i < wordChoice.length; i++){
 	// Checks if user entry is a match to any letter in  the mystery word and updates html if it is
-	if (wordChoice.indexOf(userGuess) >= 0) {
-		mysteryWord[wordChoice.indexOf(userGuess)] = userGuess;
-		document.querySelector("#mystery-word").innerHTML = mysteryWord;
+		if (wordChoice.indexOf(userGuess) >= i) {
+			mysteryWord[wordChoice.indexOf(i)] = userGuess;
+			document.querySelector("#mystery-word").innerHTML = mysteryWord;
 	// Accounts for winning condition
-		if (mysteryWord.join("") == wordChoice) {
-	 		document.querySelector("#reward").src=rewardsArr[compChoice].source;
-	 		document.querySelector("#mystery-word").innerHTML = "You have won!";
+			if (mysteryWord.join("") == wordChoice) {
+	 			document.querySelector("#reward").src=rewardsArr[compChoice].source;
+	 			document.querySelector("#mystery-word").innerHTML = "You have won!";
+	 		}
 	 	}
+	 }
 	// Covers letters that don't match any letters in the mystery word
 	} else {
-		wrongGuesses++;
+		wrongGuesses--;
+		document.querySelector("#guesses-left").innerHTML = "You have " + wrongGuesses + " wrong guesses left.";
 		console.log(wrongGuesses);
 	// Accounts for loss condition
-		if (wrongGuesses == 8) {
+		if (wrongGuesses == 0) {
 		document.querySelector("#mystery-word").innerHTML = "You have lost.";
 		}
 	}
